09149630b7b378790b5c4b13ee605d0160afb029
dogharasu <dogharasu171@gmail.com>
2022-02-19 14:50:34 +0900

    commit

--- terraform/cloud9.tf
+++ terraform/cloud9.tf
@@ -1,6 +1,7 @@
 ############################################################################
 # Cloud9
 ############################################################################
+# 参考：https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloud9_environment_ec2#type
 # Cloud9
 resource "aws_cloud9_environment_ec2" "example" {
   description   = "Example env"
